098023751134442233defdcd6431a5073530615c
Rahul Chandak <rahulschandak@gmail.com>
2023-06-12 16:51:11 -0400

    a5 REST su23

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import session from "express-session";
+import cors from "cors";
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import AuthController from "./users/auth-controller.js";
+const app = express();
+app.use(
+  session({
+    secret: "any string",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+app.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    store: new session.MemoryStore(),
+  })
+);
+app.use(express.json());
+TuitsController(app);
+HelloController(app);
+UserController(app);
+AuthController(app);
+app.listen(4000);
