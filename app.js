00bde80a90164ce7bf94980ba67ce04f2f5d602a
Rahul Chandak <rahulschandak@gmail.com>
2023-06-19 22:55:23 -0400

    a6 Mongo su23

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
+import mongoose from "mongoose";
+
+// mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 // app.use(
 //   cors({
--- users/auth-controller.js
+++ users/auth-controller.js
@@ -1,28 +1,56 @@
 import * as usersDao from "./users-dao.js";
 var currentUserVar;
 const AuthController = (app) => {
+  // const register = async (req, res) => {
+  //   const username = req.body.username;
+  //   const user = await usersDao.findUserByUsername(username);
+  //   if (user) {
+  //     res.sendStatus(409);
+  //     return;
+  //   }
+  //   const newUser = await usersDao.createUser(req.body);
+  //   currentUserVar = newUser;
+  //   res.json(newUser);
+  // };
 
   const register = async (req, res) => {
-    const username = req.body.username;
-    const user = await usersDao.findUserByUsername(username);
+    const user = await usersDao.findUserByUsername(req.body.username);
     if (user) {
       res.sendStatus(409);
       return;
     }
     const newUser = await usersDao.createUser(req.body);
+    // req.session["currentUser"] = newUser;
     currentUserVar = newUser;
     res.json(newUser);
   };
 
+  // const login = async (req, res) => {
+  //   const username = req.body.username;
+  //   const password = req.body.password;
+  //   const user = await usersDao.findUserByCredentials(username, password);
+  //   if (user) {
+  //     currentUserVar = user;
+  //     res.json(user);
+  //   } else {
+  //     res.sendStatus(404);
+  //   }
+  // };
+
   const login = async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    const user = await usersDao.findUserByCredentials(username, password);
-    if (user) {
-      currentUserVar = user;
-      res.json(user);
+    if (username && password) {
+      const user = await usersDao.findUserByCredentials(username, password);
+      if (user) {
+        // req.session["currentUser"] = user;
+        currentUserVar = user;
+        res.json(user);
+      } else {
+        res.sendStatus(403);
+      }
     } else {
-      res.sendStatus(404);
+      res.sendStatus(403);
     }
   };
 
@@ -49,7 +77,7 @@ const AuthController = (app) => {
       res.sendStatus(500);
     }
   };
-  
+
   app.post("/api/users/register", register);
   app.post("/api/users/login", login);
   app.post("/api/users/profile", profile);
