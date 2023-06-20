1a2fad4a60593a42c31702cdc9549f5ba5301a78
Rahul Chandak <rahulschandak@gmail.com>
2023-06-20 12:09:37 -0400

    a6 Mongo su23 - 2

--- app.js
+++ app.js
@@ -12,38 +12,35 @@ const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: "http://localhost:3000",
-//   })
-// );
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    "http://localhost:3000",
-    "https://a5--papaya-salamander-4aad5c.netlify.app/",
-    "https://tuiter-node-server-app-7rwr.onrender.com/api",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+// app.use((req, res, next) => {
+//   const allowedOrigins = [
+//     "http://localhost:3000",
+//     "https://a5--papaya-salamander-4aad5c.netlify.app/",
+//     "https://tuiter-node-server-app-7rwr.onrender.com/api",
+//   ];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.header("Access-Control-Allow-Origin", origin);
+//   }
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, PUT, POST, DELETE, PATCH, OPTIONS"
+//   );
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+//   next();
+// });
 
-    "GET, PUT, POST, DELETE, PATCH, OPTIONS"
-  );
-
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  res.header("Cache-Control", "no-cache, no-store, must-revalidate");
-
-  next();
-});
 app.use(
   session({
     secret: "any string",
